c70b140bb38ebaf7b3eb6bc92a8ade051b3e6361
Bob Park <hwpark@malgn.com>
2025-02-12 11:09:16 +0900

    feat: order 추가

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 module.exports = {
-    "printWidth": 120,
-    "semi": true,
-    "singleQuote": true,
-    "trailingComma": "all",
-    "tabWidth": 2,
-    "bracketSpacing": true,
-    "endOfLine": "auto",
-    "useTabs": false,
-    "importOrder": [
-        '^react$',
-        '^react-icons/(.*$)',
-        "^next$",
-        "^next/(.*)$",
-        "@/app/(.*)$",
-        "@/hooks/(.*)$",
-        '@/components/(.*)$',
-        "@/shared/(.*)$",
-        "@/entries/(.*)$",
-        "@/utils/(.*)$",
-        "<THIRD_PARTY_MODULES>",
-        "^[./]"
-    ],
-    "importOrderSeparation": true,
-    "importOrderSortSpecifiers": true,
-    "plugins": [
-        "@trivago/prettier-plugin-sort-imports",
-        "prettier-plugin-tailwindcss"
-    ]
+  "printWidth": 120,
+  "semi": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "bracketSpacing": true,
+  "endOfLine": "auto",
+  "useTabs": false,
+  "importOrder": [
+    '^react$',
+    '^react-icons/(.*$)',
+    "^next$",
+    "^next/(.*)$",
+    "@/app/(.*)$",
+    "@/domain/(.*)$",
+    "@/hooks/(.*)$",
+    '@/components/(.*)$',
+    "@/shared/(.*)$",
+    "@/entries/(.*)$",
+    "@/utils/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]"
+  ],
+  "importOrderSeparation": true,
+  "importOrderSortSpecifiers": true,
+  "plugins": [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss"
+  ]
 }
